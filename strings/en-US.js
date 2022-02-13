5a6b58cd0d5063c399f6540e60c92ba0822eb6ff
BallsyBread <44901003+BallsyBread@users.noreply.github.com>
2022-02-13 14:53:03 +0100

    send message on join to #welcome with mention of joiner

---
+++ strings/en-US.js
@@ -0,0 +1,5 @@
+module.exports = {
+    joinmessage(mentionname) {
+        return "Hello "+mentionname.toString()+". Welcome to the clan. Plese reply to this message with your IGN to recieve full access to the server.";
+    }
+}
