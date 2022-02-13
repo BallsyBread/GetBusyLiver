1f5ec3d1d6aa450b80d61d0a65914315b39efa72
BallsyBread <44901003+BallsyBread@users.noreply.github.com>
2022-02-13 16:28:48 +0100

    foolproof guildCreate

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,8 +1,34 @@
-let {joinmessage} = require("../strings/en-US");
+let {joinmessage, prerequisiteerror} = require("../strings/en-US");
+
+function leave(guild) {
+    guild.leave().then((g) => console.log("left guild"+g.toString())).catch(e => {console.log(e)});
+}
 
 module.exports = {
     name: 'guildCreate',
     async execute(guild) {
-        guild.members.fetch(guild.ownerId).then(owner => owner.createDM().then(channel => channel.send(joinmessage(owner))));
+        let guildowner;
+        let dmchannel;
+        let channels;
+
+        try {
+            // try to fetch guild owner as member, leave if not successful
+            await guild.members.fetch(guild.ownerId).then(owner => guildowner = owner);
+            // try to createDM channel with Owner, leave if not successful
+            await guildowner.createDM().then(channel => dmchannel = channel);
+            // try to fetch all guild channels, leave if not successful
+            await guild.channels.fetch().then(collection => channels = collection);
+        }
+        catch(e) {
+            //log error
+            console.log(e);
+            //leave guild on error
+            leave(guild);
+        }
+        //send Owner joinmessage
+        dmchannel.send(joinmessage(guildowner.toString()));
+
+        console.log("Bot just joined"+guild.toString()+" and created DM: "+dmchannel.toString()+" with Owner "+guildowner.user.toString());
+
     }
 };
--- strings/en-US.js
+++ strings/en-US.js
@@ -2,7 +2,10 @@ module.exports = {
     welcomemessage(user) {
         return "Hello "+user.toString()+". Welcome to the clan. Plese reply to this message with your IGN to recieve full access to the server.";
     },
-    joinmessage(user) {
-        return "Hello "+user.toString()+". Thanks for inviting me to the server. For the bot to work, there needs to be a \"member\" Role as well as a \"#welcome\" TextChannel.";
+    joinmessage(owner) {
+        return "Hello "+owner.toString()+". Thanks for inviting me to the server. For the bot to work, there needs to be a \"member\" Role as well as a \"#welcome\" TextChannel.";
+    },
+    prerequisiteerror() {
+        return "There was an error in the prerequisite check. Please try again.";
     }
 }
