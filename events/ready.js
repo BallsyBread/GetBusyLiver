05ef2403282f76a925cd41d11185d3957de9e921
BallsyBread <44901003+BallsyBread@users.noreply.github.com>
2022-02-16 15:56:44 +0100

    Update ready.js
    copied logic from guildCreate, as it does the same. will be refactored later

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,33 @@
+let {welcomechannelerror} = require("../strings/en-US");
+
+function leave(guild) {
+    guild.leave().then((g) => console.log("left guild"+g.toString())).catch(e => {console.log(e)});
+}
+
+async function checkchannels(guild) {
+    let welcomechannels = guild.channels.cache.filter(channel => channel.name === "welcome");
+
+    let guildowner = await guild.members.fetch(guild.ownerId);
+    //fetch a new DM channel with owner
+    let dmchannel = await guildowner.createDM();
+
+    if (welcomechannels.size !== 1) {
+        console.log("Too many or too few welcome channels");
+        //send owner an error message
+        dmchannel.send(welcomechannelerror);
+        //try to leave the guild
+        leave(guild);
+        //end event handler
+    }
+}
+
 module.exports = {
     name: 'ready',
     once: true,
-    execute(client) {
+    async execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
-        //TODO: add logic to remove all non bot messages from all welcome channels of all guilds
+        //check if all guilds still have only one welcome channel and leave if not the case
+        client.guilds.cache.forEach(guild => checkchannels(guild));
         //TODO: add logic to figure out if someone joined while they were gone and send them a authmessage on the welcome channel
     }
 }
