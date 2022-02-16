4e53c1fb0fe149e00305cf0dbc9aa519e7bae6d2
BallsyBread <44901003+BallsyBread@users.noreply.github.com>
2022-02-16 13:05:24 +0100

    Update messageCreate.js
    add guard clause for message being a DM, add guard clause for message not being a reply, add guard clause for reference message not cotaining a metion of the user or not being by a bot

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,28 +1,41 @@
 module.exports = {
     name: 'messageCreate',
     async execute(message) {
-        //check if message is DM or from a bot and return if so
-        if (!message.inGuild() || message.author.bot) return;
-        //fetch channels from guild this message was posted to
-        let guildchannels = await message.guild.channels.fetch();
-        //find welcomechannel of the guild
-        let welcomechannel = guildchannels.find(channel => channel.name === "welcome");
-        //check if message that was recieved is the last one in the welcome channel and return if not so
-        if (!welcomechannel.lastMessage === message) return;
-        //check if message is a reply, delete it and return if not so
-        if (message.mentions.repliedUser === null) {
-            message.delete();
+        if (message.author.bot) return;
+        if (message.channel.type === "DM") {
+            //handle dm
+            console.log("Message is a DM");
             return;
         }
-        //fetch message that this message was a reply to
-        let authmessage = await welcomechannel.messages.fetch(message.reference.messageId);
-        //check if fetched message has a mention of the author of the recieved message, set author's nick to message content and delete the reference message
-        if (authmessage.mentions.has(message.author) && authmessage.author.bot) {
-            await message.member.setNickname(message.content);
-            //TODO: logic to add user to member role
-            authmessage.delete();
+        if (message.channel.name !== "welcome") {
+            //handle message is not in welcome channel
+            console.log("Message was not sent to welcome channel");
+            return;
         }
-        //delete message
+        //handle message is in welcome channel
+        console.log("Message has been sent to welcome channel");
+        //handle message is not a reply
+        if (message.mentions.repliedUser === null) {
+            console.log("Message was not a reply");
+            message.delete();
+            console.log("Message will now be deleted");
+            return;
+        }
+        //handle message is a reply
+        console.log("Message is a reply");
+        //fetch message that this message was a reply to
+        let authmessage = await message.channel.messages.fetch(message.reference.messageId);
+        //check if fetched message has a mention of the author of the recieved message, set author's nick to message content and delete the reference message
+        if (!authmessage.mentions.has(message.author) || !authmessage.author.bot) {
+            console.log("Message was a reply to something different");
+            message.delete();
+            console.log("Message will now be deleted");
+            return;
+        }
+        console.log("Message that this message was a reply to has a mention of the author and is by a bot.");
+        await message.member.setNickname(message.content);
+        //TODO: logic to add user to member role
         message.delete();
+        authmessage.delete();
     }
 };
