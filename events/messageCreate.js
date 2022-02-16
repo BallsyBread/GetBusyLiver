47da353dbc8c9d09a0f0c3c85dd0db477fdec86e
BallsyBread <44901003+BallsyBread@users.noreply.github.com>
2022-02-16 13:55:49 +0100

    fix comments

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,41 +1,55 @@
 module.exports = {
     name: 'messageCreate',
     async execute(message) {
-        if (message.author.bot) return;
-        if (message.channel.type === "DM") {
-            //handle dm
-            console.log("Message is a DM");
+        //check if message is by bot and handle appropriately, then return if true
+        if (message.author.bot) {
+            //handle message is by a Bot
+
             return;
         }
+        //handle message is not by a Bot
+
+        //check if message is a DM and handle appropriately, then return if true
+        if (message.channel.type === "DM") {
+            //handle is a dm
+
+            return;
+        }
+        //handle message is not DM and not by a Bot
+
+        //check if message is not welcome channel and handle appropriately, then return if true
         if (message.channel.name !== "welcome") {
             //handle message is not in welcome channel
-            console.log("Message was not sent to welcome channel");
+
             return;
         }
-        //handle message is in welcome channel
-        console.log("Message has been sent to welcome channel");
-        //handle message is not a reply
+        //handle message is in welcome channel, is not a DM and is not by a Bot
+
+        //check if message is not a reply and handle appropriately, then return if true
         if (message.mentions.repliedUser === null) {
-            console.log("Message was not a reply");
+            //handle message is not a reply
+
+            //delete the message that was sent (as it's not the reply we're looking for)
             message.delete();
-            console.log("Message will now be deleted");
             return;
         }
-        //handle message is a reply
-        console.log("Message is a reply");
-        //fetch message that this message was a reply to
+        //handle message is a reply, is in welcome channel, is not a DM and is not by a Bot
+
+        //fetch message that this message was a reply to from messages in channel that this message was sent to
         let authmessage = await message.channel.messages.fetch(message.reference.messageId);
-        //check if fetched message has a mention of the author of the recieved message, set author's nick to message content and delete the reference message
+        //check if Reference Message doesn't have a mention of the author that sent a reply or isn't from a Bot and handle appropriately, then return if true
         if (!authmessage.mentions.has(message.author) || !authmessage.author.bot) {
-            console.log("Message was a reply to something different");
+            //delete the message that was sent (as it is not the reply we're looking for)
             message.delete();
-            console.log("Message will now be deleted");
             return;
         }
-        console.log("Message that this message was a reply to has a mention of the author and is by a bot.");
+        //handle message reference has mention of author, message reference is by a Bot, message is a reply, is in welcome channel and is not by a Bot
+        //set nickname to message content
         await message.member.setNickname(message.content);
         //TODO: logic to add user to member role
+        //delete the message (as it was the message we were looking for)
         message.delete();
+        //delete message reference as the user is now verified
         authmessage.delete();
     }
 };
