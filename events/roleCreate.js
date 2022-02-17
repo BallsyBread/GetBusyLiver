e5d1b6ad1714d95d9f4c966e058ef2630098c8fe
BallsyBread <44901003+BallsyBread@users.noreply.github.com>
2022-02-17 15:05:54 +0100

    failing to meet perequisites on run now reverts changes

---
+++ events/roleCreate.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'roleCreate',
+    execute(role) {
+        console.log(role.name);
+        if (!(role.name === "member")) return;
+        role.guild.fetch().then(guild => guild.members.fetch(guild.ownerId).then(owner => owner.send("As long as this Bot is active, you can not have more than one role that's called member. The Bot will now delete the created role again.")));
+        role.delete();
+    }
+}
