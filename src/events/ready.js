ca0e87d2cc018e3e9047596a076cc51e8dbb396a
BallsyBread <44901003+BallsyBread@users.noreply.github.com>
2022-03-01 14:03:02 +0100

    remove unfinished commands

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,9 @@
+const config = require("../../config.json");
+
 module.exports = {
     name: 'ready',
     once: true,
-    async execute(client) {
+    execute: function (client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
     }
 }
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const client = new Client({ intents: ["GUILDS", "DIRECT_MESSAGES", "GUILD_MESSAG
 
 client.commands = new Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
@@ -17,7 +17,7 @@ for (const file of commandFiles) {
     client.commands.set(command.data.name, command);
 }
 
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
     const event = require(`./events/${file}`);
